624e6508edd8f5fc128d779fa642ded519403d32
venumaheshuni12 <venumaheshuni@gmail.com>
2023-05-02 18:18:34 +0530

    New table Sample7 created

--- DWCode/CareTeam/Tables/R__Care_Sample.sql
+++ DWCode/CareTeam/Tables/R__Care_Sample.sql
@@ -2,7 +2,7 @@ CREATE Schema if not exists  DW_STG_POC;
 
 USE Schema DW_STG_POC;
 
-CREATE TABLE Sample6_Care_STG
+CREATE TABLE Sample7_Care_STG
 (Patient_ID NUMBER
 , P_NAME VARCHAR2(10)
 , P_ADDRESS VARCHAR2(300));
@@ -12,7 +12,7 @@ CREATE Schema if not exists  DW_TGT_POC;
 USE Schema DW_TGT_POC;
 
 
-CREATE TABLE Sample6_Care_TGT
+CREATE TABLE Sample7_Care_TGT
 (Patient_ID NUMBER
 , P_NAME VARCHAR2(10)
 , P_ADDRESS VARCHAR2(300)
