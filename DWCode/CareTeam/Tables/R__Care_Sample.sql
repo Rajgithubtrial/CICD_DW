793e8eb10d2764bc27c5ca3daf6197b94ff83d5a
venumaheshuni12 <131147093+venumaheshuni12@users.noreply.github.com>
2023-05-03 16:02:45 +0530

    Update R__Care_Sample.sql
    Added Sample8 table

--- DWCode/CareTeam/Tables/R__Care_Sample.sql
+++ DWCode/CareTeam/Tables/R__Care_Sample.sql
@@ -2,17 +2,23 @@ CREATE Schema if not exists  DW_STG_POC;
 
 USE Schema DW_STG_POC;
 
-CREATE TABLE Sample7_Care_STG
+CREATE TABLE if not exists Sample7_Care_STG
 (Patient_ID NUMBER
 , P_NAME VARCHAR2(10)
 , P_ADDRESS VARCHAR2(300));
 
-CREATE Schema if not exists  DW_TGT_POC;
+CREATE Schema if not exists DW_TGT_POC;
 
 USE Schema DW_TGT_POC;
 
 
-CREATE TABLE Sample7_Care_TGT
+CREATE TABLE if not exists Sample7_Care_TGT
+(Patient_ID NUMBER
+, P_NAME VARCHAR2(10)
+, P_ADDRESS VARCHAR2(300)
+);
+
+CREATE TABLE if not exists Sample8_Care_TGT
 (Patient_ID NUMBER
 , P_NAME VARCHAR2(10)
 , P_ADDRESS VARCHAR2(300)
