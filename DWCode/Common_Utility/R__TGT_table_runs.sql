d4cb0f9a6443b9a59b3c0a6d11c827cf15a7b5c2
venumaheshuni12 <131147093+venumaheshuni12@users.noreply.github.com>
2023-05-03 15:06:26 +0530

    Update R__TGT_table_runs.sql
    Renamed Table to R1

--- DWCode/Common_Utility/R__TGT_table_runs.sql
+++ DWCode/Common_Utility/R__TGT_table_runs.sql
@@ -2,10 +2,10 @@ CREATE Schema if not exists DW_TGT_POC;
 
 USE Schema DW_TGT_POC;
 
-create table if not exists R_TGT_Deploymnt (
+create table if not exists R1_TGT_Deploymnt (
 	Deployment VARCHAR(30),
 	Load_Date  TIMESTAMP_NTZ (9) default current_timestamp()
 
 );
 
-INSERT INTO R_TGT_Deploymnt (Deployment) VALUES ('TEST run time 2: ');
+INSERT INTO R1_TGT_Deploymnt (Deployment) VALUES ('TEST run time 2: ');
