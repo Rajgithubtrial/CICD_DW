50a446857c09ab8eed68033b879027716aac3b97
pallavi1799 <pallavimuppa@gmail.com>
2023-05-09 17:10:57 +0530

    created care 09052023

---
+++ DWCode/CareTeam/Tables/R__care_09052023.sql
@@ -0,0 +1,8 @@
+CREATE Schema if not exists  DW_STG_POC;
+
+USE Schema DW_STG_POC;
+
+CREATE TABLE if not exists Sample2_care_09052023
+(Patient_ID NUMBER
+, P_NAME VARCHAR2(10)
+, P_ADDRESS VARCHAR2(300));
