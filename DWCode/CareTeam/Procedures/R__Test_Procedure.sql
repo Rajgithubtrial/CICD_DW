e1b9ec04433607361b63904a285e9b8262f409b3
Rajgithubtrial <120726003+Rajgithubtrial@users.noreply.github.com>
2023-05-11 17:33:35 +0530

    Test_apple1
    Test_apple1

--- DWCode/CareTeam/Procedures/R__Test_Procedure.sql
+++ DWCode/CareTeam/Procedures/R__Test_Procedure.sql
@@ -11,11 +11,29 @@ var command = "INSERT INTO Employee (emp_id, emp_name, emp_address) VALUES (emp_
 return 'var';
 $$;
 
+CREATE OR REPLACE PROCEDURE Test_Prc2(name varchar, address varchar)
+RETURNS VARCHAR
+LANGUAGE JAVASCRIPT
+AS
+$$
+var command = "INSERT INTO Employee (emp_id, emp_name, emp_address) VALUES (emp_id.nextval, '"+NAME+"','"+ADDRESS+"')";
+return 'var';
+$$;
+
 CREATE Schema if not exists DW_TGT_POC;
 
 USE Schema DW_TGT_POC;
 
 CREATE OR REPLACE PROCEDURE Test_TGT_Prc(name varchar, address varchar)
+RETURNS VARCHAR
+LANGUAGE JAVASCRIPT
+AS
+$$
+var command = "INSERT INTO Employee (emp_id, emp_name, emp_address) VALUES (emp_id.nextval, '"+NAME+"','"+ADDRESS+"')";
+return 'var';
+$$;
+
+CREATE OR REPLACE PROCEDURE Test_TGT_Prc2(name varchar, address varchar)
 RETURNS VARCHAR
 LANGUAGE JAVASCRIPT
 AS
